702c970a310761664995205bdbbd199e03542ad1
coreyhall15 <114037718+coreyhall15@users.noreply.github.com>
2022-10-25 21:08:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-const body = $('body')
-console.log(body)
+onst API_KEY = "2978f26e34msh5a5fc34c35208c3p18d3c1jsn110a71f21703"
+const BASE_URL = 'love-calculator.p.rapidapi.com'
+	
+	//"url": "https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John"
