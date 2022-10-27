c80805f4bcc1e658b16de9a16c935748c56f5063
Corey Hall <cohall15@gmail.com>
2022-10-26 21:20:37 -0400

    style change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API_KEY = "2978f26e34msh5a5fc34c35208c3p18d3c1jsn110a71f21703"
-const BASE_URL = 'love-calculator.p.rapidapi.com'
+const BASE_URL = 'http"//rapidapi.com/ajith/api/love-calculator'
 	
-	//"url": "https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John"
+	//"url": "http"//rapidapi.com/ajith/api/love-calculator"
 
 //Event Listeners
 window.onload = function() {
